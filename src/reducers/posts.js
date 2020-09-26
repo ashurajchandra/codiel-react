30a601990eb0ec04ea02967a4c20458063470328
Ashutosh Rai <mukutrai12@gmail.com>
2020-09-26 12:36:12 +0530

    final edit

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,4 @@
-import { UPDATE_POSTS } from "../actions/actionTypes";
+import { UPDATE_POSTS, ADD_POST , ADD_COMMENT} from "../actions/actionTypes";
 
 
 
@@ -7,6 +7,24 @@ export default function posts(state=[],action){
     switch(action.type){
         case UPDATE_POSTS:
             return action.posts;
+
+            case ADD_POST:
+                return [action.post, ...state];
+
+
+                case ADD_COMMENT:
+                    const newPosts = state.map((post) => {
+                      if (post._id === action.postId) {
+                        return {
+                          ...post,
+                          comments: [action.comment, ...post.comments],
+                        };
+                      }
+              
+                      return post;
+                    });
+                    return newPosts;
+
             default :
             return state;
     }
